b6dcac6bdaf117c8b2cb3c44c8b264845e69f8d7
fbf <16911126+fbfqwq@user.noreply.gitee.com>
2026-08-09 18:27:47 +0800

    补充上次提交

--- src/mdtnh/MainMod.java
+++ src/mdtnh/MainMod.java
@@ -6,8 +6,8 @@ import arc.util.Log;
 
 import mdtnh.energy.MdtEnergyBlocks;
 import mdtnh.energy.MdtEnergySystem;
-import mdtnh.ui.build.MdtBuildMenuContent;
-import mdtnh.ui.build.MdtBuildMenuFragment;
+import mdtnh.modui.build.MdtBuildMenuContent;
+import mdtnh.modui.build.MdtBuildMenuFragment;
 
 import mindustry.game.EventType.ClientLoadEvent;
 import mindustry.mod.Mod;
