faafbc6954f920d1b53818a7671b4985bcead9cc
BaCu-Si <2607586807@qq.com>
2026-08-11 11:35:38 +0800

    改bug。。。

--- src/mdtnh/ModItems.java
+++ src/mdtnh/ModItems.java
@@ -80,7 +80,31 @@ public class ModItems {
             // 杆
             items.put(metal + "_bar", new Item(metal + "-bar", base.cpy().mul(1.1f)) {{ cost = 1.8f; }});
         }
+        for (int i = 0; i < ALLOYS.length; i++) {
+            String metal = ALLOYS[i];
+            Color base= Pal.darkMetal;
+            //应急措施。。。
 
+            // 锭
+            items.put(metal + "_ingot", new Item(metal + "-ingot", base) {{ cost = 1.2f; }});
+            // 块
+            items.put(metal + "_block", new Item(metal + "-block", base.cpy().mul(0.7f)) {{ cost = 3.0f; }});
+            // 粒
+            items.put(metal + "_granule", new Item(metal + "-granule", base.cpy().mul(1.2f)) {{ cost = 0.2f; }});
+            // 粉
+            items.put(metal + "_powder", new Item(metal + "-powder", base.cpy().mul(0.9f)) {{ cost = 0.5f; }});
+            // 小堆粉
+            items.put(metal + "_small-pile-powder", new Item(metal + "-small-pile-powder", base.cpy().mul(0.8f)) {{ cost = 0.15f; }});
+            // 小撮粉
+            items.put(metal + "_pinch-powder", new Item(metal + "-pinch-powder", base.cpy().mul(0.7f)) {{ cost = 0.08f; }});
+            // 板
+            items.put(metal + "_plate", new Item(metal + "-plate", base.cpy().mul(1.1f)) {{ cost = 1.8f; }});
+            // 箔
+            items.put(metal + "_foil", new Item(metal + "-foil", base.cpy().mul(1.3f)) {{ cost = 0.8f; }});
+
+            // 杆
+            items.put(metal + "_bar", new Item(metal + "-bar", base.cpy().mul(1.1f)) {{ cost = 1.8f; }});
+        }
         // 特殊物品（可选）
         items.put("nano_swarm", new Item("nano-swarm", Color.valueOf("00FFAA")) {{
             radioactivity = 0.7f;
